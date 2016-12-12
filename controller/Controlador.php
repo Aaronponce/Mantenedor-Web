37c63f00ec794bc7873a2d5adf27382f8a99c4ae
Aaron Ponce Sandoval <aaron.c.ponce@gmail.com>
2016-12-12 02:32:49 -0300

    Controlador del ingreso de registros a la BD

---
+++ controller/Controlador.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once ('../model/Conexion.php');
+require_once ('../model/Registro.php');
+
+$id = "";
+$publicada = "21/56/89";
+$fecha = "21/56/89";
+$ingreso = "21/56/89";
+$nombre = "aaron";
+$email = "as@gmail";
+$carrera = "civil";
+$men_principal = "mencion";
+$prf_guia = "profe guia";
+$prf_cor1 = "core1";
+$prf_cor2 = "core2";
+$titulo = "titulo";
+$resumen = "resumen";
+
+$Conexion = new Conexion();
+$Registro = new Registro($id, $publicada, $fecha, $ingreso, $nombre, $nombre, $email, $carrera, $men_principal, $prf_guia, $prf_cor1, $prf_cor2, $titulo, $resumen);
+
+$Conexion->consulta($Registro->agregar());
+
+
+?>
